61832c703e80e5a0da92ec88041b40e0cf53c028
alex mtx <alex-mtx@users.noreply.github.com>
2023-03-22 18:02:28 +0100

    comment test

--- Entities/ICosmosAggregate.cs
+++ Entities/ICosmosAggregate.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Azure.Cosmos;
 
 namespace Common.CosmosDbServices.Entities;
-
+/// <summary>
+/// Marks an entity as an Aggregate
+/// </summary>
 public interface ICosmosAggregate
 {
     string Id { get; }
