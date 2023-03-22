928cd2aa98aed9bbd7704deac8c70b9ac1b3b427
alex mtx <alex-mtx@users.noreply.github.com>
2023-03-22 17:59:16 +0100

    initial

---
+++ Entities/ICosmosAggregate.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Azure.Cosmos;
+
+namespace Common.CosmosDbServices.Entities;
+
+public interface ICosmosAggregate
+{
+    string Id { get; }
+    PartitionKey PartitionKey { get; }
+}
