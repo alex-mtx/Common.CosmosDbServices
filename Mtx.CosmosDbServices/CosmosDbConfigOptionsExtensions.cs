280821f7729684754ca8f19a7e20b19a6ae4f173
alex mtx <alex-mtx@users.noreply.github.com>
2023-06-17 17:10:41 +0200

    cosmetic

--- Mtx.CosmosDbServices/CosmosDbConfigOptionsExtensions.cs
+++ Mtx.CosmosDbServices/CosmosDbConfigOptionsExtensions.cs
@@ -1,47 +1,44 @@
-﻿
-using Microsoft.Azure.Cosmos;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
-using System.Configuration;
 
 namespace Mtx.CosmosDbServices
 {
-    public static class CosmosDbConfigOptionsExtensions
-    {
-        /// <summary>
-        /// Creates a Cosmos DB database and a container with the specified partition key. 
-        /// </summary>
-        /// <returns></returns>
-        public static void InitializeCosmosDbService(this IServiceCollection services, IConfiguration configuration)
-        {
+	public static class CosmosDbConfigOptionsExtensions
+	{
+		/// <summary>
+		/// Creates a Cosmos DB database and a container with the specified partition key. 
+		/// </summary>
+		/// <returns></returns>
+		public static void InitializeCosmosDbService(this IServiceCollection services, IConfiguration configuration)
+		{
 			services.Configure<CosmosDbOptions>(configuration.GetSection(CosmosDbOptions.CosmosDb));
 
-            services.AddTransient<ICosmosDbService, CosmosDbService>();
-            services.AddSingleton<IContainerFactory, ContainerFactory>();
-            services.AddSingleton<CosmosClient>(factory =>
-            {
-                var options = factory.GetRequiredService<IOptions<CosmosDbOptions>>().Value;
+			services.AddTransient<ICosmosDbService, CosmosDbService>();
+			services.AddSingleton<IContainerFactory, ContainerFactory>();
+			services.AddSingleton<CosmosClient>(factory =>
+			{
+				var options = factory.GetRequiredService<IOptions<CosmosDbOptions>>().Value;
 
-                var clientOptions = new CosmosClientOptions
-                {
-                    //SerializerOptions = options,
-                    Serializer = new CosmosJsonDotNetSerializer(new JsonSerializerSettings
-                    {
+				var clientOptions = new CosmosClientOptions
+				{
+					//SerializerOptions = options,
+					Serializer = new CosmosJsonDotNetSerializer(new JsonSerializerSettings
+					{
 
-                        ContractResolver = new JsonDotNetPrivateResolver(),
-                        TypeNameHandling = TypeNameHandling.None,
-                        ReferenceLoopHandling = ReferenceLoopHandling.Error,
-                        PreserveReferencesHandling = PreserveReferencesHandling.None,
-                        ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor,
-                    })
+						ContractResolver = new JsonDotNetPrivateResolver(),
+						TypeNameHandling = TypeNameHandling.None,
+						ReferenceLoopHandling = ReferenceLoopHandling.Error,
+						PreserveReferencesHandling = PreserveReferencesHandling.None,
+						ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor,
+					})
 
-                };
+				};
 
-                return new CosmosClient(options.Endpoint, options.Key,clientOptions: clientOptions);
-            });
-        }
-    }
+				return new CosmosClient(options.Endpoint, options.Key, clientOptions: clientOptions);
+			});
+		}
+	}
 
 }
